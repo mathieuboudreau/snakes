14ff17498844377c5e75bb293d223ac0c34a2b2b
mathieuboudreau <emb6150@gmail.com>
2016-11-05 19:31:10 -0230

    Create Snake function, position him to center

--- snakes.js
+++ snakes.js
@@ -1,7 +1,26 @@
+var player;
+var PSIZE = 10;
+
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(500, 500);
+    player = new Snake();
 }
 
 function draw() {
-    background(200)
+    background(200);
+    player.update();
+    player.show();
+}
+
+function Snake() {
+    this.x = width/2;
+    this.y = height/2;
+
+    this.update = function() {
+    }
+
+    this.show = function() {
+        fill(0);
+        rect(this.x, this.y, PSIZE, PSIZE);
+    }
 }
